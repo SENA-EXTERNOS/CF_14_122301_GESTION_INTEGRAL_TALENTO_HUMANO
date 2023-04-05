98fd25d930d3624c3853f50a1ba79a595c6ea612
Andrea Botello <83100148+abotello2@users.noreply.github.com>
2023-04-05 12:54:25 -0500

    Update Introduccion.vue

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -25,7 +25,6 @@
        figure
       .video
         iframe(width="560" height="315" src="https://www.youtube.com/embed/VD75SvAkIrM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption   VIDEO
     
 </template>
 
